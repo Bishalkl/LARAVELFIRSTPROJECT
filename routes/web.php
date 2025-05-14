be5fc52e0f66ce7e23043e8d079b8b7b2e5c3f01
bishalkl <bishal123koirala@gmail.com>
2025-05-14 22:43:18 +0545

    done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// normal routes
+Route::get('/home', function() {
+    return view('home');
+});
+
+// Named routes
+Route::get('/dashboard', function(){
+    return view('dashboard');
+})->name('dashboard');
+
+
+// Route Parameters
+Route::get('/user/{id}', function($id) {
+    return "User ID:$id";
+});
+
+// Optional parameter
+Route::get('/users/{name?}', function($name = "Bishal") {
+    return "User Name: $name";
+});
+
+// Routes Groups
+// For applying middleware, prefix, or namespace
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profile', function() {
+        return view('profile');
+    });
+});

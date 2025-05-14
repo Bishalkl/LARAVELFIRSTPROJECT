c54192f92a36e972d57780d946c5eed9b60b1640
bishalkl <bishal123koirala@gmail.com>
2025-05-15 00:09:22 +0545

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>This is Home</h1>
-    <a href="{{ route('dashboard')}}">Dashboard</a>
-</body>
-</html>
+<h1>{{$name}}</h1>
+<a href="{{route('userProfileName', ['name' => $name])}}">Name of Profile</a>
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,84 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// normal routes which return the view of blade file 
 Route::get('/', function () {
     return view('welcome');
 });
 
+// create get request if we use we can trigger any kind of request
+Route::any('/login', function() {
+        // return "hi"; simple return something
 
-// normal routes
-Route::get('/home', function() {
-    return view('home');
+        // using response for and using it 
+        // return response("<h1>Hello, I am login</h1>", 200, ["Content-Type" => "text"]);
+
+        // Instead of using that arrow use header function using application content type
+        return response("<h1>Hello, I am login</h1>", 200)->header("Content-Type", "application/json");
+
+
 });
 
-// Named routes
-Route::get('/dashboard', function(){
-    return view('dashboard');
-})->name('dashboard');
+
+// redirect routes
+// redirect r1 to r2
+Route::redirect('/r1', '/r2', 301);
+// create route for r2 and it's working redirected to r2 if we clicked to r1 path 
+// status code for redirect is 301 for Moved Permanently redirect and 302 is temporarily redirect
+Route::get('/r2', function() {
+    return "This is r2 route";
+});
+
+// View route using view to targe the blade file and with url
+Route::view('/welcome', 'welcome');
+// in View route we can also pass the value
+Route::view('/home', 'home', ['name'=> 'Bishal']);
 
 
 // Route Parameters
-Route::get('/user/{id}', function($id) {
-    return "User ID:$id";
+
+// Requred Parameters adding constrant by where 
+Route::get('/user/{id?}', function($id=4){
+    return $id;
+})->where('id', '[0-9]+');
+
+
+// Named Routes
+Route::get('/user/profile/{name?}', function($name='bishal'){
+    return "My name is {$name}.";
+})->name("userProfileName");
+
+// Routes Group while using prefix and middleware auth for authentication and guest for normal
+Route::prefix("user")->middleware("guest")->group(function () {
+    Route::view('/home', 'home', ['name' => 'Bishal']);
 });
 
-// Optional parameter
-Route::get('/users/{name?}', function($name = "Bishal") {
-    return "User Name: $name";
+// if we gave the name the route with prefexi we should use .at the end in prefix value the and 
+Route::prefix("admin.")->group(function() {
+    Route::get('/users', function() {
+        return 'admin.user';
+    })->name("userss");
 });
 
-// Routes Groups
-// For applying middleware, prefix, or namespace
-Route::middleware(['auth'])->group(function () {
-    Route::get('/profile', function() {
-        return view('profile');
-    });
+
+// cache routes
+// php artisan route;cache
+
+// clear routes
+// php artisan route:clear
+
+
+
+// Request class and use Illuminates\Http\Request; and use dd for detail in browser
+Route::get("search", function(Request $request) {
+    dd($request->name);
 });
+
+// To integrate the api.php for apis build, have to manually add to the web.php for this 
+Route::prefix('api')->middleware('api')->group(base_path('routes/api.php'));
+
+
+
+

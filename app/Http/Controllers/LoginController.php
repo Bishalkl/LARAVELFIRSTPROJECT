49c0a0e2b9be3be5a4524ed2b345686c487b2194
bishalkl <bishal123koirala@gmail.com>
2025-05-16 14:11:41 +0545

    done

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginFormRequest;
 
 class LoginController extends Controller
 {
@@ -12,21 +13,13 @@ class LoginController extends Controller
     }
 
     //method for get value
-    public function store(Request $request) {
+    public function store(LoginFormRequest $request) {
 
-
-        // manually validation for validate it 
-         $request->validate([
-            'username' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|string|min:8',
-        ]);
         // // access all the value of request
         // $value = $request->all();
 
         // flash message
         return redirect()->back()->with('success', 'form successfully submitted');
-
 
 
         // // Redirect to home route with flashed session data
